1617c649298807500b807b6a8a33f755ee6bf763
rufer7 <rufermarc@gmail.com>
2018-08-13 14:22:02 +0200

    First draft (skeleton) of Open-Visio and Open-Visio.Tests added

---
+++ src/Open-Visio.Tests.ps1
@@ -0,0 +1,81 @@
+#Requires -Modules @{ ModuleName = 'biz.dfch.PS.Pester.Assertions'; ModuleVersion = '1.1.1' }
+
+$here = Split-Path -Parent $MyInvocation.MyCommand.Path;
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".");
+
+Describe "Open-Visio" {
+	
+	. "$here\$sut";
+	# . "$here\Close-Visio.ps1";
+	
+	Context "Open-Visio-ValidationTests" {
+		It "Warmup" -Test {
+			
+			# Arrange
+			
+			# Act
+			
+			# Assert
+			$true | Should Be $true;
+		}
+		
+		It "ThrowsParameterBindingValidationExceptionWhenInvokingWithNullPath" {
+			
+			# Arrange
+
+			# Act
+			{ Open-Visio -Path $null; } | Should ThrowException 'ParameterBindingValidationException';
+
+			# Assert
+		}
+		
+		It "ThrowsParameterBindingValidationExceptionWhenInvokingWithNotExistingPath" {
+			
+			# Arrange
+
+			# Act
+			{ Open-Visio -Path "C:\arbitrary"; } | Should ThrowException 'ParameterBindingValidationException';
+
+			# Assert
+		}
+	}
+	
+	Context "Open-Visio-PositiveTests" {
+		
+		$pathToVisioDoc = "$here\SampleVisio.vsd";
+		
+		BeforeEach {
+			$visioDoc = Open-Visio -Path $pathToVisioDoc;
+		}
+		
+		It "OpensAndReturnsVisioDocWhenInvokingWithValidPathToExistingVisioDoc" {
+			
+			# Arrange
+			
+			# Act
+			
+			# Assert
+			$visioDoc | Should Not Be $null;
+		}
+		
+		AfterEach {
+			$null = Close-Visio $visioDoc;
+		}
+	}
+}
+
+#
+# Copyright 2018 d-fens GmbH
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
---
+++ src/Open-Visio.ps1
@@ -0,0 +1,72 @@
+#Requires -Modules @{ ModuleName = 'biz.dfch.PS.System.Logging'; ModuleVersion = '1.4.1' }
+
+Function Open-Visio {
+<#
+.SYNOPSIS
+
+Opens a specific Microsoft Visio document.
+
+.DESCRIPTION
+
+Opens a specific Microsoft Visio document.
+
+The path to the Microsoft Visio document has to be provided as input by either positional or named parameter of type string.
+
+.EXAMPLE
+
+$visio = Open-Visio C:\PATH\TO\MyVisio.vsd
+
+Path to the Microsoft Visio document is passed as a positional parameter to the Cmdlet.
+
+.LINK
+
+GitHub Repository: https://github.com/dfensgmbh/biz.dfch.PS.Visio.Scripts
+
+#>
+
+[CmdletBinding(
+    SupportsShouldProcess = $true
+	,
+    ConfirmImpact = "Low"
+)]
+PARAM
+(
+	[ValidateScript( { Test-Path($_); } )]
+	[ValidateNotNullOrEmpty()]
+	[Parameter(Mandatory = $true, Position = 0)]
+	[string] $Path
+)
+
+BEGIN
+{
+	trap { Log-Exception $_; break; }
+
+	$OutputParameter = $null;
+}
+
+PROCESS
+{
+	trap { Log-Exception $_; break; }
+}
+
+END
+{
+	return $OutputParameter;
+}
+}
+
+#
+# Copyright 2018 d-fens GmbH
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
