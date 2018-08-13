c1363274197a840953fba640e1fa6219c75ab4a6
rufer7 <rufermarc@gmail.com>
2018-08-13 16:22:16 +0200

    Get-Page function implemented

---
+++ src/Get-Page.Tests.ps1
@@ -0,0 +1,108 @@
+#Requires -Modules @{ ModuleName = 'biz.dfch.PS.Pester.Assertions'; ModuleVersion = '1.1.1' }
+
+$here = Split-Path -Parent $MyInvocation.MyCommand.Path;
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".");
+
+Describe "Get-Page" {
+	
+	. "$here\$sut";
+	. "$here\Open-Visio.ps1";
+	. "$here\Close-Visio.ps1";
+	
+	Context "Get-Page-ValidationTests" {
+		It "Warmup" -Test {
+			
+			# Arrange
+			
+			# Act
+			
+			# Assert
+			$true | Should Be $true;
+		}
+		
+		It "ThrowsParameterBindingValidationExceptionWhenInvokingWithNullVisioDoc" {
+			
+			# Arrange
+
+			# Act
+			{ Get-Page -VisioDoc $null; } | Should ThrowException 'ParameterBindingValidationException';
+
+			# Assert
+		}
+		
+		It "ThrowsParameterBindingValidationExceptionWhenInvokingWithNullName" {
+			
+			# Arrange
+			$visioDoc = New-Object -ComObject Scripting.Dictionary;
+			
+			# Act
+			{ Get-Page -VisioDoc $visioDoc -Name $null; } | Should ThrowException 'ParameterBindingValidationException';
+			
+			#Assert
+		}
+	}
+	
+	Context "Get-Page-PositiveTests" {
+		
+		$pathToVisioDoc = "$here\SampleVisio.vsdx";
+		
+		BeforeEach {
+			$visioDoc = Open-Visio -Path $pathToVisioDoc;
+		}
+		
+		It "RetrievesAndReturnsListOfAvailablePagesOfVisioDocumentWhenInvokingWithValidOpenedVisioDocument" {
+			
+			# Arrange
+			
+			# Act
+			$result = Get-Page $visioDoc;
+			
+			# Assert
+			$result | Should Not Be $null;
+			$result.Count | Should Be 2;
+		}
+
+		It "RetrievesPageByNameAndReturnsSpecifiedPageOfVisioDocumentWhenInvokingWithValidOpenedVisioDocumentAndExistingPageName" {
+			
+			# Arrange
+			
+			# Act
+			$result = Get-Page $visioDoc -Name "Page-2";
+			
+			# Assert
+			$result | Should Not Be $null;
+			$result.Name | Should Be "Page-2";
+		}
+		
+		It "ReturnsNullWhenInvokingWithValidOpenedVisioDocumentAndNotExistingPageName" {
+			
+			# Arrange
+			
+			# Act
+			$result = Get-Page $visioDoc -Name "Page-42";
+			
+			# Assert
+			$result | Should Be $null;
+		}
+		
+		AfterEach {
+			$null = Close-Visio $visioDoc;
+		}
+	}
+}
+
+#
+# Copyright 2018 d-fens GmbH
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
---
+++ src/Get-Page.ps1
@@ -0,0 +1,94 @@
+#Requires -Modules @{ ModuleName = 'biz.dfch.PS.System.Logging'; ModuleVersion = '1.4.1' }
+
+Function Get-Page {
+<#
+.SYNOPSIS
+
+Retrieves one or more page objects from the specified Visio document.
+
+.DESCRIPTION
+
+Retrieves one or more page objects from the specified Visio document.
+
+The Visio document has to be provided as input by either positional or named parameter.
+
+.EXAMPLE
+
+Get-Page $visioDoc
+
+Visio document is passed as a positional parameter to the Cmdlet.
+
+.LINK
+
+GitHub Repository: https://github.com/dfensgmbh/biz.dfch.PS.Visio.Scripts
+
+#>
+
+[CmdletBinding(
+    SupportsShouldProcess = $true
+	,
+    ConfirmImpact = "Low"
+	,
+	DefaultParameterSetName = 'list'
+)]
+PARAM
+(
+	[ValidateNotNull()]
+	[Parameter(Mandatory = $true, Position = 0)]
+	$VisioDoc
+	,
+	# Full name or part of it, for the Visio page you want to search - this is not case sensitive
+	[Parameter(Mandatory = $false, ParameterSetName = 'searchByName')]
+	[ValidateNotNullOrEmpty()]
+	[String] $Name = $null
+	,
+	# Lists all available pages of the Visio document
+	[Parameter(Mandatory = $false, ParameterSetName = 'list')]
+	[Switch] $ListAvailable
+)
+
+BEGIN
+{
+	trap { Log-Exception $_; break; }
+}
+
+PROCESS
+{
+	trap { Log-Exception $_; break; }
+	
+	if($PSCmdlet.ParameterSetName -eq 'list') 
+	{
+		$result = $VisioDoc.Pages;
+	}
+	else
+	{
+		If ($PSCmdlet.ParameterSetName -eq 'SearchByName')
+		{
+			$result = $VisioDoc.Pages |? Name -match $Name;
+		}
+	}
+	
+	$OutputParameter = $result;
+}
+
+END
+{	
+	return $OutputParameter;
+}
+}
+
+#
+# Copyright 2018 d-fens GmbH
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
