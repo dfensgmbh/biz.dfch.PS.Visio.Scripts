b70fd7d1642e63e8efb7fbc5b7e9bf348c082ec3
rufer7 <rufermarc@gmail.com>
2018-08-20 10:37:02 +0200

    Get-Shape function implemented

---
+++ src/Get-Shape.Tests.ps1
@@ -0,0 +1,137 @@
+#Requires -Modules @{ ModuleName = 'biz.dfch.PS.Pester.Assertions'; ModuleVersion = '1.1.1' }
+
+$here = Split-Path -Parent $MyInvocation.MyCommand.Path;
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".");
+
+Describe "Get-Shape" {
+	
+	. "$here\$sut";
+	. "$here\Open-VisioDocument.ps1";
+	. "$here\Get-Page";
+	. "$here\Close-VisioDocument.ps1";
+	
+	Context "Get-Shape-ValidationTests" {
+		It "Warmup" -Test {
+			
+			# Arrange
+			
+			# Act
+			
+			# Assert
+			$true | Should Be $true;
+		}
+		
+		It "ThrowsParameterBindingValidationExceptionWhenInvokingWithNullPage" {
+			
+			# Arrange
+
+			# Act
+			{ Get-Shape -Page $null; } | Should ThrowException 'ParameterBindingValidationException';
+
+			# Assert
+		}
+		
+		It "ThrowsParameterBindingValidationExceptionWhenInvokingWithNullName" {
+			
+			# Arrange
+			$page = New-Object -ComObject Scripting.Dictionary;
+			
+			# Act
+			{ Get-Shape -Page $page -Name $null; } | Should ThrowException 'ParameterBindingValidationException';
+			
+			#Assert
+		}
+		
+		It "ThrowsParameterBindingValidationExceptionWhenInvokingWithEmptyName" {
+			
+			# Arrange
+			$page = New-Object -ComObject Scripting.Dictionary;
+			
+			# Act
+			{ Get-Shape -Page $page -Name ""; } | Should ThrowException 'ParameterBindingValidationException';
+			
+			#Assert
+		}
+	}
+	
+	Context "Get-Shape-PositiveTests" {
+		
+		$pathToVisioDoc = "$here\SampleVisio.vsdx";
+		$pageName = "Page-1";
+		$eaGuid = [guid]::newGuid();
+		
+		BeforeEach {
+			$visioDoc = Open-VisioDocument -Path $pathToVisioDoc;
+			$page = Get-Page $pageName;
+			Add-ShapeToPage -VisioDoc $visioDoc -Page $page -PositionX 1.0 -PositionY 1.0 -Height 1.0 -Width 1.0 -EaGuid $eaGuid;
+			$shape = Add-ShapeToPage -VisioDoc $visioDoc -Page $page -PositionX 1.0 -PositionY 1.0 -Height 1.0 -Width 1.0;
+		}
+		
+		It "RetrievesAndReturnsListOfAvailableShapesOfVisioPageWhenInvokingWithValidVisioPage" {
+			
+			# Arrange
+			
+			# Act
+			$result = Get-Shape $page;
+			
+			# Assert
+			$result | Should Not Be $null;
+			$result.Count | Should Be 2;
+		}
+
+		It "RetrievesShapeByNameAndReturnsSpecifiedShapeOfVisioPageWhenInvokingWithValidVisioPage" {
+			
+			# Arrange
+			
+			# Act
+			$result = Get-Shape $page -Name $shape.Name;
+			
+			# Assert
+			$result | Should Not Be $null;
+			$result.Name | Should Be $shape.Name;
+		}
+		
+		It "RetrievesShapeByEaGuidAndReturnsSpecifiedShapeOfVisioPageWhenInvokingWithValidVisioPage" {
+			
+			# Arrange
+			
+			# Act
+			$result = Get-Shape $page -EaGuid $eaGuid;
+			
+			# Assert
+			$result | Should Not Be $null;
+			$result.Data1 | Should Be $eaGuid;
+		}
+		
+		It "ReturnsNullWhenInvokingWithValidVisioPageAndNotExistingShapeName" {
+			
+			# Arrange
+			
+			# Act
+			$result = Get-Shape $page -Name "arbitrary";
+			
+			# Assert
+			$result | Should Be $null;
+		}
+		
+		AfterEach {
+			$null = Close-VisioDocument $visioDoc;
+		}
+	}
+}
+
+#
+# Copyright 2018 d-fens GmbH
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
---
+++ src/Get-Shape.ps1
@@ -0,0 +1,101 @@
+#Requires -Modules @{ ModuleName = 'biz.dfch.PS.System.Logging'; ModuleVersion = '1.4.1' }
+
+Function Get-Shape {
+<#
+.SYNOPSIS
+
+Retrieves one or more shapes from the specified page.
+
+.DESCRIPTION
+
+Retrieves one or more shapes from the specified page.
+
+The page has to be provided as input by either positional or named parameter.
+
+.EXAMPLE
+
+Get-Shape $page
+
+Page is passed as a positional parameters to the Cmdlet.
+
+.LINK
+
+GitHub Repository: https://github.com/dfensgmbh/biz.dfch.PS.Visio.Scripts
+
+#>
+
+[CmdletBinding(
+    SupportsShouldProcess = $true
+	,
+    ConfirmImpact = "Low"
+	,
+	DefaultParameterSetName = 'list'
+)]
+PARAM
+(
+	[ValidateNotNull()]
+	[Parameter(Mandatory = $true, Position = 0)]
+	$Page
+	,
+	# Full name or part of it, for the shape you want to search - this is not case sensitive
+	[Parameter(Mandatory = $false, ParameterSetName = 'searchByName')]
+	[ValidateNotNullOrEmpty()]
+	[String] $Name
+	,
+	[Parameter(Mandatory = $false, ParameterSetName = 'searchByEaGuid')]
+	[guid] $EaGuid
+	,
+	# Lists all available shapes of the specified page of the given Microsoft Visio document
+	[Parameter(Mandatory = $false, ParameterSetName = 'list')]
+	[Switch] $ListAvailable
+)
+
+BEGIN
+{
+	trap { Log-Exception $_; break; }
+}
+
+PROCESS
+{
+	trap { Log-Exception $_; break; }
+	
+	if($PSCmdlet.ParameterSetName -eq 'list') 
+	{
+		$result = $Page.Shapes;
+	}
+	else
+	{
+		if ($PSCmdlet.ParameterSetName -eq 'SearchByName')
+		{
+			$result = $Page.Shapes |? Name -match $Name;
+		}
+		if ($PSCmdlet.ParameterSetName -eq 'searchByEaGuid')
+		{
+			$result = $Page.Shapes |? Data1 -match $EaGuid;
+		}
+	}
+	
+	$OutputParameter = $result;
+}
+
+END
+{	
+	return $OutputParameter;
+}
+}
+
+#
+# Copyright 2018 d-fens GmbH
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
